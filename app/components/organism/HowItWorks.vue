4949bfa41d0073c18fd7f5bcbfeae093d0cce7d8
Dmytro <johndoe@example.com>
2025-08-01 14:04:57 +0300

    hiw made

--- app/components/organism/HowItWorks.vue
+++ app/components/organism/HowItWorks.vue
@@ -3,10 +3,39 @@
     <h2 class="hiw__title">
       How it works <span class="hiw__subtitle">(Steps)</span>
     </h2>
+
+    <div class="steps">
+      <div v-for="step in steps" :key="step.label" class="steps__item">
+        <div>{{ step.label }}</div>
+        <div>{{ step.description }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const steps = [
+  {
+    label: "1.Upload Your Tracks",
+    description: "Drag and drop any number of audio files - MP3, WAV, or FLAC.",
+  },
+  {
+    label: "2.We Mix Them Like a Real DJ",
+    description:
+      "Our engine analyzes tempo, key, and structure to create smooth transitions.",
+  },
+  {
+    label: "3.Download a Seamless Mix",
+    description:
+      "You get one single, professionally blended file - ready for any venue.",
+  },
+  {
+    label: "4.Enjoy the Music - Effortlessly",
+    description:
+      "Press play and let your perfectly mixed soundtrack set the mood.",
+  },
+];
+</script>
 
 <style lang="scss" scoped>
 .hiw {
@@ -19,7 +48,7 @@
     text-transform: uppercase;
     margin-bottom: 111px;
   }
-  
+
   &__subtitle {
     position: absolute;
     color: var(--txt-accent-color);
@@ -29,6 +58,35 @@
     top: 12px;
     text-transform: capitalize;
     font-weight: normal;
+  }
+
+  .steps {
+    width: 100%;
+    height: 480px;
+    display: flex;
+    align-items: start;
+    gap: 20px;
+    background-image: url("img/hiw-bg.png");
+    background-repeat: no-repeat;
+    background-size: 325px;
+    background-position: center;
+    &__item {
+      padding: 28px 28px 42px 28px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      width: max(23%, 283px);
+      aspect-ratio: 1;
+      border-radius: 30px;
+      background: var(--bg-secondary-color);
+
+      &:nth-child(2) {
+        align-self: flex-end;
+      }
+      &:nth-child(3) {
+        align-self: center;
+      }
+    }
   }
 }
 </style>

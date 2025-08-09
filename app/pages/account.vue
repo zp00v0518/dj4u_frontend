d025888dc66d6d575d2532116cf24d55b685c2dc
Dmytro <johndoe@example.com>
2025-08-09 13:16:28 +0300

    added plans

--- app/pages/account.vue
+++ app/pages/account.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="account" v-if="profile">
-    <h1 class="account__greeting">Welcome, &nbsp; {{ profile.fullName }}!</h1>
-    <h4 class="account__subtitle">Choose your plan to start mixing</h4>
+    <div class="account__main">
+      <h1 class="account__greeting">Welcome, &nbsp; {{ profile.fullName }}!</h1>
+      <h4 class="account__subtitle">Choose your plan to start mixing</h4>
 
-    <Plans />
+      <Plans />
+    </div>
   </div>
 </template>
 
@@ -23,16 +25,29 @@ onMounted(() => {
 
 <style lang="scss" scoped>
 .account {
-  background-image: url("/img/main-banner.png");
-  background-size: cover;
-  background-repeat: no-repeat;
-  height: 100dvh;
-  margin-top: calc(0px - var(--header-height));
-  margin-left: calc(0px - var(--padding-side-main));
-  margin-right: calc(0px - var(--padding-side-main));
-  padding-top: calc(30px + var(--header-height));
-  padding-right: 45px;
-  padding-left: 45px;
+  --account-padding-side: 45px;
+
+  padding-right: var(--account-padding-side);
+  padding-left: var(--account-padding-side);
+
+  &__main {
+    background-image: url("/img/main-banner.png");
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 100dvh;
+    margin-top: calc(0px - var(--header-height));
+    margin-left: calc(
+      0px - var(--padding-side-main) - var(--account-padding-side)
+    );
+    margin-right: calc(
+      0px - var(--padding-side-main) - var(--account-padding-side)
+    );
+    padding-top: calc(30px + var(--header-height));
+    padding-right: var(--account-padding-side);
+    padding-left: var(--account-padding-side);
+    display: flex;
+    flex-direction: column;
+  }
 
   &__greeting {
     font-family: "Martian Mono";
@@ -44,6 +59,11 @@ onMounted(() => {
 
   &__subtitle {
     font-weight: 400;
+  }
+
+  .plans {
+    margin-top: auto;
+    margin-bottom: rem(88px);
   }
 }
 </style>
